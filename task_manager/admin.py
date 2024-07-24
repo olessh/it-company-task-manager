1eb1fe4bfb08ae65675866443517577db7ba32b2
Oles Shulzhenko <simplemanua@gmail.com>
2024-07-24 11:03:05 +0300

    Register Position, TaskType, Task and Worker models in Django admin
    Configuring Task model administration with filters, search fields, and list display.
    Enhanced Worker model admin to include position field, with updated fieldsets for adding and editing.

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Position, TaskType, Task, Worker
+
+admin.site.register(Position)
+
+admin.site.register(TaskType)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ["task_type", "priority",]
+    search_fields = ["name",]
+    list_display = [
+        "name",
+        "task_type",
+        "priority",
+        "deadline",
+        "is_completed"
+    ]
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_filter = ["position", ]
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": (
+            "last_name",
+            "first_name",
+            "position",
+        )}),
+    )
