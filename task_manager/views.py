371a6db62015637f473ec7e676df3b0a407a595b
Oles Shulzhenko <simplemanua@gmail.com>
2024-07-24 17:46:33 +0300

    Add index view to count and display numbers of positions, task types, tasks, and workers

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from task_manager.models import Position, TaskType, Task, Worker
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_positions = Position.objects.count()
+    num_task_types = TaskType.objects.count()
+    num_tasks = Task.objects.count()
+    num_workers = Worker.objects.count()
+
+    context = {
+        "num_positions": num_positions,
+        "num_task_types": num_task_types,
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
