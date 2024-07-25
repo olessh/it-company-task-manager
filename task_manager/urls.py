f5142123263a41fb0d6eb28298d3b4d80e7b1909
Oles Shulzhenko <simplemanua@gmail.com>
2024-07-25 11:08:18 +0300

    Add URL patterns for Position, TaskType, Task, and Worker views with corresponding names

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,32 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from task_manager.views import index
+from task_manager.views import (
+    index,
+    PositionListView,
+    TaskTypeListView,
+    TaskListView,
+    TaskDetailView,
+    WorkerListView,
+    WorkerDetailView
+)
 
 app_name = "task_manager"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list"
+    ),
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list"
+    ),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
