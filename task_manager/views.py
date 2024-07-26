5ba7d43093c9bdfc2db8355d607d8d00cc18cd6f
Oles Shulzhenko <simplemanua@gmail.com>
2024-07-26 10:40:07 +0300

    Add authorization to access pages and count user visits

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import generic
@@ -5,51 +7,55 @@ from django.views import generic
 from task_manager.models import Position, TaskType, Task, Worker
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_positions = Position.objects.count()
     num_task_types = TaskType.objects.count()
     num_tasks = Task.objects.count()
     num_workers = Worker.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_positions": num_positions,
         "num_task_types": num_task_types,
         "num_tasks": num_tasks,
         "num_workers": num_workers,
+        "num_visits": num_visits + 1
     }
 
     return render(request, "task_manager/index.html", context=context)
 
 
-class PositionListView(generic.ListView):
+class PositionListView(LoginRequiredMixin, generic.ListView):
     model = Position
     paginate_by = 5
 
 
-class TaskTypeListView(generic.ListView):
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
     model = TaskType
     template_name = "task_manager/task_type_list.html"
     context_object_name = "task_type_list"
     paginate_by = 5
 
 
-class TaskListView(generic.ListView):
+class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     queryset = Task.objects.select_related("task_type")
     paginate_by = 5
 
 
-class TaskDetailView(generic.DetailView):
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
     template_name = "task_manager/task_detail.html"
 
 
-class WorkerListView(generic.ListView):
+class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
     paginate_by = 5
 
 
-class WorkerDetailView(generic.DetailView):
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
     model = Worker
     queryset = Worker.objects.select_related("position").prefetch_related("tasks", "tasks__task_type")
     template_name = "task_manager/worker_detail.html"
