f7bfa7c2870932508c5a898223f9c07b2e6b02b2
Oles Shulzhenko <simplemanua@gmail.com>
2024-07-26 10:44:06 +0300

    Add URL pattern for including Django authentication

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("task_manager.urls", namespace="task_manager"))
+    path("", include("task_manager.urls", namespace="task_manager")),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
