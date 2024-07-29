ebb32c920602d519a8e9d7d82b09d608f473c1f9
Oles Shulzhenko <simplemanua@gmail.com>
2024-07-29 10:58:33 +0300

    Add URL path create, update, delete for Position, TaskType, Task

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,9 +4,18 @@ from django.urls import path
 from task_manager.views import (
     index,
     PositionListView,
+    PositionCreateView,
+    PositionUpdateView,
+    PositionDeleteView,
     TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
     TaskListView,
     TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
     WorkerListView,
     WorkerDetailView
 )
@@ -21,12 +30,45 @@ urlpatterns = [
         name="position-list"
     ),
     path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create",
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
+    path(
         "task-types/",
         TaskTypeListView.as_view(),
         name="task-type-list"
     ),
+    path(
+        "task-types/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create",
+    ),
+    path(
+        "task-types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update",
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete",
+    ),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
