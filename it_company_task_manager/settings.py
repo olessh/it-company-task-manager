06574d3d9a4148877c06658df21c1959bdd76263
Oles Shulzhenko <simplemanua@gmail.com>
2024-09-18 17:07:19 +0300

    UPD DEBUG

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
